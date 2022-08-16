38b7535ca028e4a2100f93e9e23404dcc2ce7433
WiseJade <104193237+WiseJade@users.noreply.github.com>
2022-08-16 23:17:57 +0900

    회원 조회 기능 구현

---
+++ entrepreCommunity/build/generated/sources/annotationProcessor/java/main/com/wisejade/api/member/mapper/MemberMapperImpl.java
@@ -0,0 +1,42 @@
+package com.wisejade.api.member.mapper;
+
+import com.wisejade.api.member.dto.MemberResponseDto;
+import com.wisejade.api.member.entity.Member;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-08-16T22:57:01+0900",
+    comments = "version: 1.5.1.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.jar, environment: Java 17.0.2 (Eclipse Adoptium)"
+)
+@Component
+public class MemberMapperImpl implements MemberMapper {
+
+    @Override
+    public MemberResponseDto memberToMemberResponseDto(Member member) {
+        if ( member == null ) {
+            return null;
+        }
+
+        MemberResponseDto memberResponseDto = new MemberResponseDto();
+
+        return memberResponseDto;
+    }
+
+    @Override
+    public List<MemberResponseDto> membersToMemberResponseDtos(List<Member> members) {
+        if ( members == null ) {
+            return null;
+        }
+
+        List<MemberResponseDto> list = new ArrayList<MemberResponseDto>( members.size() );
+        for ( Member member : members ) {
+            list.add( memberToMemberResponseDto( member ) );
+        }
+
+        return list;
+    }
+}
